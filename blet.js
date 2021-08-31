d78e0f33510b6e812e6b494496fbbf46dc2d48d0
Ilya Furman <smashlong@gmail.com>
2021-08-31 12:37:36 +0300

    Add server and bookmarklet

---
+++ blet.js
@@ -0,0 +1,24 @@
+tn = orderDataResponse.orderData.salesOrders
+  .map(order =>
+    order.shippingPackages.map(pkg => pkg.deliveryInfo.trackingNumber)
+  )
+  .flat()
+  .join()
+
+iod = new URL(window.location).searchParams.get('purchaseOrderId')
+url = `https://nigguntry.ngrok.io/add/${iod}&${tn}`
+
+i = new Image()
+i.src = url
+document.body.append(i)
+window.location.replace(`#✅${tn}`)
+
+// https://www.ebay.com/vod/FetchOrderDetails?ViewPaymentStatus&purchaseOrderId=25-0751-385029
+
+// https://chriszarate.github.io/bookmarkleter/
+
+/*
+
+javascript:tn=orderDataResponse.orderData.salesOrders.map(a=>a.shippingPackages.map(a=>a.deliveryInfo.trackingNumber)).flat().join(),iod=new URL(window.location).searchParams.get("purchaseOrderId"),url=`https://nigguntry.ngrok.io/add/${iod}&${tn}`,i=new Image,i.src=url,document.body.append(i),window.location.replace(`#✅${tn}`);
+
+*/
