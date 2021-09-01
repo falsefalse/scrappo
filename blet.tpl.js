163c0fdc8b2d86adf882fdf950abfde6501a5801
Ilya Furman <smashlong@gmail.com>
2021-09-01 17:14:30 +0300

    Better texts

--- blet.tpl.js
+++ blet.tpl.js
@@ -13,12 +13,14 @@ i.src = url
 document.body.append(i)
 window.location.replace(`#✅${tn}`)
 
-// https://www.ebay.com/vod/FetchOrderDetails?ViewPaymentStatus&purchaseOrderId=25-0751-385029
-
-// https://chriszarate.github.io/bookmarkleter/
+// https://chriszarate.github.io/bookmarkleter/ - minifier
 
 /*
 
+👇👇👇 copy this into your bookmark
+
 javascript:tn=orderDataResponse.orderData.salesOrders.map(a=>a.shippingPackages.map(a=>a.deliveryInfo.trackingNumber)).flat().join(),oid=new URL(window.location).searchParams.get("purchaseOrderId"),url=`%%APP_URL%%/add/${oid}&${tn}`,i=new Image,i.src=url,document.body.append(i),window.location.replace(`#✅${tn}`);
+
+👆👆👆
 
 */
